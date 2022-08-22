08af16d7c3f946f13fd2ec120adec7a23a06f4a3
CaptainHoldMyBeer <luperkalfan@gmail.com>
2022-08-22 15:19:36 +0300

    work sync

---
+++ EventPlanning/ClientApp/src/app/login/login.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core'
+
+@Component({
+    selector: 'login.component',
+    templateUrl: './login.component.html'
+})
+export class LoginComponent {
+    public user: any;
+}
